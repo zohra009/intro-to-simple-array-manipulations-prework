c52197af24e2612902bb5895f45122533ce3749b
anzohra@outlook.com <anzohra@outlook.com>
2018-06-21 18:34:25 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -28,9 +28,12 @@ end
 def using_flatten(array)
   array.flatten 
 end
-def using_delete_at(array)
+def using_delete(array, string)
+  array.delete(string)
+end
+def using_delete_at(array, string)
   array.delete_at()
-
+end
 
 
 
