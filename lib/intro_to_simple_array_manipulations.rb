0f8fe06bc2705b1ecf8c96bdae93de5b17d0c025
anzohra@outlook.com <anzohra@outlook.com>
2018-06-21 18:36:21 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -20,7 +20,7 @@ def using_concat(array1, array2)
   array1.concat(array2)
 end
 def using_insert(array, item)
-  array.insert(item) 
+  array.insert(4, item) 
 end 
 def using_uniq(array)
   array.uniq 
