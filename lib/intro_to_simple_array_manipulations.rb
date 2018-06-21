63ea7d55f5580d575147a9f07e4b0d1b23f6ee60
anzohra@outlook.com <anzohra@outlook.com>
2018-06-21 18:26:17 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -16,8 +16,8 @@ end
 def shift_with_args(array)
   array.shift(2)
 end 
-def using_concat(array)
-  array.concat 
+def using_concat(array1, array2)
+  array1.concat(array2)
 end
 def using_insert(array)
   array.insert 
