f3da3f2b2957e1a471b773794d9952e318accbe9
anzohra@outlook.com <anzohra@outlook.com>
2018-06-21 18:35:43 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -4,8 +4,8 @@ end
 def using_unshift(array, string)
   array.unshift(string)
 end
-def using_pop(array, string)
-  array.pop(string)
+def using_pop(array)
+  array.pop
 end
 def pop_with_args(array)
   array.pop(2)
